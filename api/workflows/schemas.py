c5ae288dc279a7a07b44fb65aeb56f264a4bd8ea
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-04 18:40:24 +0300

    adding title for workflow

--- api/workflows/schemas.py
+++ api/workflows/schemas.py
@@ -6,9 +6,10 @@ class WorkflowBase(BaseModel):
 
 
 class WorkflowCreate(WorkflowBase):
-    pass
+    title: str
 
 
 class Workflow(WorkflowBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
+    title: str
--- core/models/workflow.py
+++ core/models/workflow.py
@@ -1,7 +1,9 @@
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 from .base import Base
 
 
 class Workflow(Base):
+    title = Column(String, nullable=True)
     nodes = relationship("Node", back_populates="workflow")
