2ef1be462f4d3aadd4ba946f0888fe38a98fe721
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-04 18:23:05 +0300

    validators were added

--- api/nodes/schemas.py
+++ api/nodes/schemas.py
@@ -1,5 +1,13 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from enum import Enum
+
+
+class MessageNodeStatus(str, Enum):
+    PENDING = "pending"
+    SENT = "sent"
+    OPENED = "opened"
 
 
 class NodeType(str, Enum):
@@ -15,11 +23,15 @@ class NodeBase(BaseModel):
 
 class NodeCreate(NodeBase):
     workflow_id: int
+    status: Optional[str]
+    message_text: Optional[str]
 
 
 class Node(NodeBase):
     id: int
     workflow_id: int
+    status: Optional[str]
+    message_text: Optional[str]
 
     class Config:
         orm_mode = True
---
+++ api/nodes/validators.py
@@ -0,0 +1,28 @@
+from fastapi import HTTPException
+from starlette import status
+
+from api.nodes.schemas import NodeCreate, NodeType
+
+
+async def validate_node_type(node_in: NodeCreate) -> None:
+    if node_in.type not in NodeType:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Invalid node type provided",
+        )
+
+
+async def validate_status(node_in: NodeCreate) -> None:
+    if node_in.type != NodeType.MESSAGE and node_in.status is not None:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Status can be provided only for nodes with 'Message Node' type",
+        )
+
+
+async def validate_message(node_in: NodeCreate) -> None:
+    if node_in.type != NodeType.MESSAGE and node_in.message_text is not None:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Message text can be provided only for nodes with 'Message Node' type",
+        )
