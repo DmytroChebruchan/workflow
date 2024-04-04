522768fb20608ed6a8a3e4e3f10014a478cc058d
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-04 18:23:35 +0300

    models were revised

--- core/models/node.py
+++ core/models/node.py
@@ -7,4 +7,6 @@ from .base import Base
 class Node(Base):
     type = Column(String)
     workflow_id = Column(Integer, ForeignKey("workflows.id"))
+    status = Column(String, nullable=True)
+    message_text = Column(String, nullable=True)
     workflow = relationship("Workflow", back_populates="nodes")
