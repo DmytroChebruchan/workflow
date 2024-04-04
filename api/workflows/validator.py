2d49f1f1bc1a390d9ba898ca64bebbbe639c54df
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-04 19:08:38 +0300

    validator was added

---
+++ api/workflows/validator.py
@@ -0,0 +1,18 @@
+from fastapi import HTTPException, status, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.workflows import crud
+from core.models import db_helper
+
+
+async def workflow_validator(
+    workflow_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    workflow = await crud.get_workflow_by_id(session, workflow_id)
+    if workflow is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Workflow with ID {workflow_id} not found",
+        )
+    return workflow
