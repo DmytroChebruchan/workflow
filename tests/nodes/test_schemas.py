b5804dca6d6eae23532e58eb63241551ee7119d0
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-07 19:15:29 +0300

    tests were fixed

--- tests/nodes/test_schemas.py
+++ tests/nodes/test_schemas.py
@@ -13,16 +13,16 @@ from tests.nodes.fixture_nodes_dicts import (
 def test_node_create():
     node_data = dummy_node
     node = NodeCreate(**node_data)
-    assert node.type == dummy_node.type
-    assert node.workflow_id == dummy_node.workflow_id
+    assert node.type == dummy_node["type"]
+    assert node.workflow_id == dummy_node["workflow_id"]
 
 
 def test_node_update():
     node_data = dummy_start_node
     node = NodeUpdate(**node_data)
-    assert node.id == dummy_start_node.id
-    assert node.workflow_id == dummy_start_node.workflow_id
-    assert node.type == dummy_start_node.type
+    assert node.id == dummy_start_node["id"]
+    assert node.workflow_id == dummy_start_node["workflow_id"]
+    assert node.type == dummy_start_node["type"]
 
 
 def test_node_invalid_data():
