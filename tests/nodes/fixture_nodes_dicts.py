7a9ae1a8300cdc981ddc5da6731b24f86c53ef96
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-07 19:15:41 +0300

    reformatting

--- tests/nodes/fixture_nodes_dicts.py
+++ tests/nodes/fixture_nodes_dicts.py
@@ -1,5 +1,9 @@
 dummy_node = {"type": "Start Node", "workflow_id": 1}
-dummy_start_node = {"type": "Start Node", "workflow_id": 1, "id": 1}
+dummy_start_node = {
+    "type": "Start Node",
+    "workflow_id": 1,
+    "id": 1,
+}
 dummy_message_node = {
     "type": "Message Node",
     "workflow_id": 1,
