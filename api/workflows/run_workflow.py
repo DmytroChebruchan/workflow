4637f9f873329b583f9f764e5625b3f3cd1af6c4
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-07 20:04:43 +0300

    run workflow endpoint was added

---
+++ api/workflows/run_workflow.py
@@ -0,0 +1,2 @@
+async def run(workflow):
+    return {"status": "success"}
--- api/workflows/views.py
+++ api/workflows/views.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import db_helper
+from .run_workflow import run
 from .schemas import Workflow, WorkflowCreate, WorkflowUpdate
 from . import crud
 from .validator import workflow_validator
@@ -24,8 +25,8 @@ async def create_workflow(
     return await crud.create_workflow(session=session, workflow_in=workflow_in)
 
 
-@router.get("/{workflow_id}/", response_model=Workflow)
-async def get_workflows(
+@router.get("/read/{workflow_id}/", response_model=Workflow)
+async def get_workflow_by_id(
     workflow_id: int,
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
@@ -42,7 +43,25 @@ async def get_workflows(
     )
 
 
-@router.put("/{workflow_id}/", response_model=Workflow)
+@router.get("/run/{workflow_id}/", response_model=Workflow)
+async def get_workflows(
+    workflow_id: int,
+    session: AsyncSession = Depends(db_helper.session_dependency),
+):
+    workflow = await crud.get_workflow_by_id(
+        session=session,
+        workflow_id=workflow_id,
+    )
+    if workflow is not None:
+        return run(workflow)
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"Workflow {workflow_id} not found!",
+    )
+
+
+@router.put("/update/{workflow_id}/", response_model=Workflow)
 async def update_workflow(
     workflow_id: int,
     workflow_update: WorkflowUpdate,
@@ -60,10 +79,12 @@ async def update_workflow(
     return updated_workflow
 
 
-@router.delete("/{workflow_id}/", response_model=None)
+@router.delete("/delete/{workflow_id}/", response_model=None)
 async def delete_workflow(
     workflow_id: int,
     session: AsyncSession = Depends(db_helper.session_dependency),
 ):
-    workflow = await workflow_validator(session, workflow_id)
+    workflow = await workflow_validator(
+        session=session, workflow_id=workflow_id
+    )
     await crud.delete_workflow_by_id(session=session, workflow=workflow)
