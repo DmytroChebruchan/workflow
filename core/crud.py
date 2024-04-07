1ff6af64a764ab087b1b4a4bb4ca3af6535898fd
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-07 19:05:54 +0300

    deleted not used file

--- core/crud.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-
-from api.models.schemas import NodeBase
-
-
-def create_new_node(node_in: NodeBase) -> dict:
-    node = node_in.model_dump()
-    return {"success": True,
-            "node": node}
