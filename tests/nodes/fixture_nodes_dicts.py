4522c8ad5651d4c2de1789412b2897d2c3857cc4
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-07 19:01:22 +0300

    fixture file for node tests was added

---
+++ tests/nodes/fixture_nodes_dicts.py
@@ -0,0 +1,11 @@
+dummy_node = {"type": "Start Node", "workflow_id": 1}
+dummy_start_node = {"type": "Start Node", "workflow_id": 1, "id": 1}
+dummy_message_node = {
+    "type": "Message Node",
+    "workflow_id": 1,
+    "status": "pending",
+}
+dummy_condition_node = {
+    "type": "Condition Node",
+    "workflow_id": 1,
+}
