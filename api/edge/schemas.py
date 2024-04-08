2b284bfc8ad55d420cc1c2f33d85680a9208f0ff
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-08 17:54:53 +0300

    bugfix addition condition None

--- api/edge/schemas.py
+++ api/edge/schemas.py
@@ -6,5 +6,5 @@ from pydantic import BaseModel, ConfigDict
 class EdgeBase(BaseModel):
     source_node_id: int
     destination_node_id: int
-    condition: Optional[bool]
+    condition: Optional[bool] = None
     model_config = ConfigDict()
