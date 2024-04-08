d648e9cc7a1a7fa0e106c55eba66790dcebc1bae
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-08 15:18:33 +0300

    adding health check view to roater

--- api/__init__.py
+++ api/__init__.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from .nodes.views import router as node_router
 from .workflows.views import router as workflow_router
+from .general.views import router as general_router
 
 router = APIRouter()
 router.include_router(router=workflow_router, prefix="/workflows")
 router.include_router(router=node_router, prefix="/nodes")
+router.include_router(router=general_router, prefix="")
