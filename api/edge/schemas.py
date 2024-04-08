0ddba4d15388689c1850bef94b26e92c347cddac
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-08 16:54:40 +0300

    schemas for edge was created

---
+++ api/edge/schemas.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+from pydantic import BaseModel, ConfigDict
+
+
+class EdgeBase(BaseModel):
+    source_node_id: int
+    destination_node_id: int
+    condition: Optional[bool]
+    model_config = ConfigDict()
