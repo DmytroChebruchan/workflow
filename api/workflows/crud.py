74a33d4a49b96dff9ec2887b790b6432743831bb
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-08 17:54:00 +0300

    adding default start and end nodes to create workflow script

--- api/workflows/crud.py
+++ api/workflows/crud.py
@@ -3,8 +3,9 @@ from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api.edge.schemas import EdgeBase
 from api.workflows.schemas import WorkflowCreate, WorkflowUpdate
-from core.models import Workflow
+from core.models import Edge, Node, Workflow
 
 
 async def get_workflows(session: AsyncSession) -> list[Workflow]:
@@ -23,10 +24,31 @@ async def get_workflow_by_id(
 async def create_workflow(
     session: AsyncSession, workflow_in: WorkflowCreate
 ) -> Workflow:
+    # Create and add the workflow to the session
     workflow = Workflow(**workflow_in.model_dump())
     session.add(workflow)
     await session.commit()
     await session.refresh(workflow)
+
+    # Create start and end nodes associated with the workflow
+    start_node = Node(type="Start Node", workflow_id=workflow.id)
+    end_node = Node(type="End Node", workflow_id=workflow.id)
+    session.add_all([start_node, end_node])
+    await session.commit()
+
+    # Create an edge connecting the start and end nodes
+    edge = Edge(
+        source_node_id=start_node.id,
+        destination_node_id=end_node.id,
+    )
+    session.add(edge)
+    await session.commit()
+
+    # Refresh objects to get their updated attributes
+    await session.refresh(start_node)
+    await session.refresh(end_node)
+    await session.refresh(edge)
+
     return workflow
 
 
