50beaf0c8bb4018d77b10b68767842b33b92fff1
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-10 12:44:20 +0300

    first test

---
+++ tests/mock_approach/nodes/test_create_start_node.py
@@ -0,0 +1,26 @@
+from api.nodes.crud import create_node
+from api.nodes.schemas.schemas import NodeCreate
+from core.models import Node
+
+import pytest
+from unittest.mock import MagicMock, AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_create_node():
+
+    dummy_node = {
+        "type": "Start Node",
+        "workflow_id": 1,
+        "id_of_true_condition": 2,
+    }
+    mock_node_in = NodeCreate(**dummy_node)
+
+    # Mock the methods of the session
+    mock_session = AsyncMock()
+
+    # Call the function
+    result = await create_node(mock_session, mock_node_in)
+
+    # Assertions
+    assert isinstance(result, Node)
