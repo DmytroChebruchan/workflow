b81136349ed69348570c148f4c815bc18987aba8
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-15 19:35:55 +0300

    refactoring

--- api/workflows/workflow_utils.py
+++ api/workflows/workflow_utils.py
@@ -10,21 +10,7 @@ async def create_workflow_with_nodes(
 
     workflow = Workflow(**workflow_in.model_dump())
     session.add(workflow)
-    await session.flush()
-
-    # Create the end node and add it to the workflow
-    end_node = Node(type="End Node", workflow_id=workflow.id)
-    session.add(end_node)
-    await session.flush()
-    await session.refresh(end_node)
-
-    # Create the start node and add it to the workflow
-    start_node = Node(
-        type="Start Node",
-        workflow_id=workflow.id,
-        id_of_true_condition=end_node.id,
-    )
-    session.add(start_node)
-    await session.flush()
+    await session.commit()
+    await session.refresh(workflow)
 
     return workflow
