1730c81359699ace93a6b799c60309bb91b5dd34
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-16 16:56:35 +0300

    alembic refactoring

---
+++ core/models/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["Base", "Node", "Edge", "Workflow"]
+
+from core.models.base import Base
+from core.models.node import Node
+from core.models.edge import Edge
+from core.models.workflow import Workflow
