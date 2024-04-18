4f2352907309f1a200c8fde2c33a9ea8d2d5d46e
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-18 17:40:33 +0300

    adding node_validator

--- api/nodes/validators.py
+++ api/nodes/validators.py
@@ -1,10 +1,8 @@
-from typing import Union
-
 from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from api.nodes.node_attr_values import MessageNodeStatus, NodeType
-from api.nodes.schemas.schemas import NodeCreate, NodeUpdate
+from api.general.utils import get_element_by_id
 from core.models.node import Node
 
 
@@ -14,3 +12,13 @@ async def validate_existence_of_node(node: Node) -> None:
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Node with ID {node.id} not found",
         )
+
+
+async def node_validator(node_id: int, session: AsyncSession) -> Node:
+    node = await get_element_by_id(
+        session=session,
+        element_id=node_id,
+        element=Node,
+    )
+    await validate_existence_of_node(node)
+    return node
