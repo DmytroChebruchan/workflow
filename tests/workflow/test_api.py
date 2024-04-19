733d1cde2735d5385f1c555a62da0e30797ead2a
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-19 16:54:30 +0300

    test for list of workflows was added

--- tests/workflow/test_api.py
+++ tests/workflow/test_api.py
@@ -6,3 +6,10 @@ async def test_workflow_created(client):
     data = response.json()
     assert data["title"] == "Test Workflow"
     assert "id" in data
+
+
+async def test_show_workflows(client):
+    response = client.get("/workflows/show_workflows/")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data == []
