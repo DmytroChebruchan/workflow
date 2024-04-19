842d0738337b8e55d363a9060e4235d1b7cdcfa7
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-20 00:43:20 +0300

    edges router was added

--- api/__init__.py
+++ api/__init__.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from .general.views import router as general_router
 from .nodes.views import router as node_router
 from .workflows.views import router as workflow_router
+from .edges.views import router as edge_router
 
 router = APIRouter()
 router.include_router(router=workflow_router, prefix="/workflows")
 router.include_router(router=node_router, prefix="/nodes")
+router.include_router(router=edge_router, prefix="/edges")
 router.include_router(router=general_router, prefix="")
