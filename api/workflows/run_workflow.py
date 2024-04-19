44f328088363d923d7ac130ca5888efbfb7ca737
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-20 00:56:00 +0300

    adding pseudo code for reference

--- api/workflows/run_workflow.py
+++ api/workflows/run_workflow.py
@@ -8,21 +8,28 @@ from core.models.workflow import Workflow
 
 
 async def run_workflow(session: AsyncSession, workflow_id: int) -> dict | None:
+    # get workflow
     result = await session.execute(
         select(Workflow)
         .where(Workflow.id == workflow_id)
         .options(selectinload(Workflow.nodes))
     )
     workflow = result.scalars().first()
-    if workflow:
-        nodes = [
-            {
-                "id": node.id,
-                "type": node.type,
-                "status": node.status,
-                "message_text": node.message_text,
-            }
-            for node in workflow.nodes
-        ]
-        return {"id": workflow.id, "title": workflow.title, "nodes": nodes}
-    return None
+
+    # get nodes
+    if not workflow:
+        return None
+
+    nodes = [
+        {
+            "id": node.id,
+            "type": node.type,
+            "status": node.status,
+            "message_text": node.message_text,
+        }
+        for node in workflow.nodes
+    ]
+    # get edges
+    # create_graph
+    # generate reply
+    return {"id": workflow.id, "title": workflow.title, "nodes": nodes}
