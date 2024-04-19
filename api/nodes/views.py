14c7aef91c7cbd379702fad6d64d2a3cbdb79e3e
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-19 23:39:01 +0300

    update view was added

--- api/nodes/views.py
+++ api/nodes/views.py
@@ -5,38 +5,37 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.general.utils import get_element_by_id, get_elements
 from api.nodes import crud
-from api.nodes.schemas.schemas import Node, NodeCreate
-from api.workflows.validator import workflow_validator
+from api.nodes.schemas.schemas import NodeFromDB, NodeCreate
 from core.database.database import get_async_session
 from core.models.node import Node as NodeModel
 
 router = APIRouter(tags=["Nodes"])
 
 
-@router.get("/show_nodes/", response_model=List[Node])
+@router.get("/show_nodes/", response_model=List[NodeFromDB])
 async def get_nodes_view(
     session: AsyncSession = Depends(get_async_session),
 ):
     return await get_elements(session=session, element=NodeModel)
 
 
-@router.post("/create/", response_model=Node)
+@router.post("/create/", response_model=NodeFromDB)
 async def create_node_view(
     node_in: NodeCreate,
     session: AsyncSession = Depends(get_async_session),
-) -> Node | None:
+) -> NodeFromDB | None:
     return await crud.create_node(session=session, node_in=node_in)
 
 
-@router.get("/details/{node_id}/", response_model=Node)
+@router.get("/details/{node_id}/", response_model=NodeFromDB)
 async def get_node_view(
     node_id: int,
     session: AsyncSession = Depends(get_async_session),
-) -> Node:
+) -> NodeFromDB:
     node = await get_element_by_id(
         session=session,
         element_id=node_id,
-        element=Node,
+        element=NodeFromDB,
     )
     if node:
         return node
@@ -52,4 +51,13 @@ async def delete_node_view(
     session: AsyncSession = Depends(get_async_session),
 ) -> Response:
     await crud.delete_node_by_id(session=session, node_id=node_id)
+    return Response(content={"status": "ok"}, status_code=200)
+
+
+@router.put("/{node_id}/")
+async def update_node_view(
+    node_id: int,
+    session: AsyncSession = Depends(get_async_session),
+) -> Response:
+    await crud.update_node(session=session, node_id=node_id)
     return Response(content={"status": "ok"}, status_code=200)
