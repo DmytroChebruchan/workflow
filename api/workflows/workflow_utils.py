b119e032c0549c60c368899d9c611922812c9632
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-19 23:15:12 +0300

    creating nodes and edge for new workflow

--- api/workflows/workflow_utils.py
+++ api/workflows/workflow_utils.py
@@ -1,16 +1,28 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.general.utils import save_element_into_db
+from api.nodes.crud import create_node
+from api.nodes.schemas.schemas import NodeCreate
+from api.workflows.crud import create_workflow
 from api.workflows.schemas import WorkflowCreate
-from core.models.node import Node
-from core.models.workflow import Workflow
+from core.models import Node
 
 
 async def create_workflow_with_nodes(
     session: AsyncSession, workflow_in: WorkflowCreate
 ):
 
-    workflow = Workflow(**workflow_in.model_dump())
-    await save_element_into_db(session=session, element=workflow)
+    workflow = await create_workflow(session, workflow_in)
+
+    # creating nodes with edges
+    start_node = NodeCreate(workflow_id=workflow.id, type="Start Node")
+    created_node = await create_node(session=session, node_in=start_node)
+
+    end_node = NodeCreate(
+        workflow_id=workflow.id,
+        type="End Node",
+        from_node_id=created_node.id,
+    )
+    end_node = await create_node(session=session, node_in=end_node)
 
     return workflow
