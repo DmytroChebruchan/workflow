5ca31ec9c5a2a0d0c46d47cf3bda3f5209b37851
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-20 00:51:24 +0300

    refactoring

--- api/workflows/workflow_utils.py
+++ api/workflows/workflow_utils.py
@@ -1,11 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.general.utils import save_element_into_db
 from api.nodes.crud import create_node
 from api.nodes.schemas.schemas import NodeCreate
 from api.workflows.crud import create_workflow
 from api.workflows.schemas import WorkflowCreate
-from core.models import Node
 
 
 async def create_workflow_with_nodes(
@@ -14,15 +12,15 @@ async def create_workflow_with_nodes(
 
     workflow = await create_workflow(session, workflow_in)
 
-    # creating nodes with edges
-    start_node = NodeCreate(workflow_id=workflow.id, type="Start Node")
-    created_node = await create_node(session=session, node_in=start_node)
+    # creating start and end nodes
+    start_node_info = NodeCreate(workflow_id=workflow.id, type="Start Node")
+    created_start_node = await create_node(session=session, node_in=start_node_info)
 
-    end_node = NodeCreate(
+    end_node_info = NodeCreate(
         workflow_id=workflow.id,
         type="End Node",
-        from_node_id=created_node.id,
+        from_node_id=created_start_node.id,
     )
-    end_node = await create_node(session=session, node_in=end_node)
+    await create_node(session=session, node_in=end_node_info)
 
     return workflow
