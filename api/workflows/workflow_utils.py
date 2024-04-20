923187eecc6e86d9e6b8cab6b2ed494968653162
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-20 15:32:41 +0300

    refactoring of type of nodes input

--- api/workflows/workflow_utils.py
+++ api/workflows/workflow_utils.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.nodes.crud import create_node
+from api.nodes.node_attr_values import NodeType
 from api.nodes.schemas.schemas import NodeCreate
 from api.workflows.crud import create_workflow
 from api.workflows.schemas import WorkflowCreate
@@ -13,14 +14,14 @@ async def create_workflow_with_nodes(
     workflow = await create_workflow(session, workflow_in)
 
     # creating start and end nodes
-    start_node_info = NodeCreate(workflow_id=workflow.id, type="Start Node")
+    start_node_info = NodeCreate(workflow_id=workflow.id, type=NodeType.START)
     created_start_node = await create_node(
         session=session, node_in=start_node_info
     )
 
     end_node_info = NodeCreate(
         workflow_id=workflow.id,
-        type="End Node",
+        type=NodeType.END,
         from_node_id=created_start_node.id,
     )
     await create_node(session=session, node_in=end_node_info)
