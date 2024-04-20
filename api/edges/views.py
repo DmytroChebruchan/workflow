d62b19389fb5123d0150b3490d67b52795bd0e22
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-20 15:33:17 +0300

    updating edge update input requirements

--- api/edges/views.py
+++ api/edges/views.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.edges.crud import delete_edge, update_edge
+from api.edges.schemas import EdgeBase
 from core.database.database import get_async_session
 
 router = APIRouter(tags=["Edges"])
@@ -10,9 +11,14 @@ router = APIRouter(tags=["Edges"])
 @router.put("update/{edge_id}/")
 async def update_view(
     edge_id: int,
+    update_edge_in: EdgeBase,
     session: AsyncSession = Depends(get_async_session),
 ) -> Response:
-    await update_edge(session=session, edge_id=edge_id)
+    await update_edge(
+        session=session,
+        edge_id=edge_id,
+        edge_update=update_edge_in,
+    )
     return Response(content={"status": "ok"}, status_code=200)
 
 
