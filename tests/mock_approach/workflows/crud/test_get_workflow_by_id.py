9fb4e1ae3d922b12bfd7d5ba08410f4e6b005e21
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-26 00:44:55 +0300

    delete and update workflow tests were added

--- tests/mock_approach/workflows/crud/test_get_workflow_by_id.py
+++ tests/mock_approach/workflows/crud/test_get_workflow_by_id.py
@@ -1,6 +1,14 @@
-from unittest.mock import AsyncMock, patch
-
-from api.workflows.crud import get_workflow_by_id
+from unittest.mock import AsyncMock, patch, MagicMock
+import unittest
+from api.workflows.crud import (
+    get_workflow_by_id,
+    update_workflow,
+    delete_workflow_by_id,
+    create_workflow,
+)
+from api.workflows.schemas import WorkflowUpdate
+from core.models import Workflow
+from tests.mock_file import true_returner_mock
 
 
 async def get_element_by_id_mock(**kwargs):
@@ -13,3 +21,29 @@ async def test_get_workflow_by_id():
     assert (
         await get_workflow_by_id(session=mock_session, workflow_id=1) is True
     )
+
+
+@patch("api.workflows.crud.update_element_id_checker", true_returner_mock)
+@patch("api.workflows.crud.commit_and_refresh_element", true_returner_mock)
+async def test_update_workflow(client):
+    workflow_update_mock = WorkflowUpdate(id=1, title="new title")
+    workflow_base_mock = Workflow(id=1, title="title")
+    session = AsyncMock()
+    function_return = await update_workflow(
+        workflow_update=workflow_update_mock,
+        workflow=workflow_base_mock,
+        session=session,
+    )
+    assert isinstance(function_return, Workflow)
+    assert function_return.id == workflow_update_mock.id
+    assert function_return.title == "new title"
+
+
+@patch("api.workflows.crud.delete_element_from_db", true_returner_mock)
+async def test_delete_workflow():
+    workflow_base_mock = Workflow(id=1, title="title")
+    session = AsyncMock()
+    result = await delete_workflow_by_id(
+        session=session, workflow=workflow_base_mock
+    )
+    assert result is None
