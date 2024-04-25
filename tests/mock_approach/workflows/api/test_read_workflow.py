7db9802611f9500e56c5243318276f571d545db7
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-25 15:46:43 +0300

    fixing test

--- tests/mock_approach/workflows/api/test_read_workflow.py
+++ tests/mock_approach/workflows/api/test_read_workflow.py
@@ -4,10 +4,13 @@ import pytest
 from fastapi.testclient import TestClient
 
 from core.models import Workflow
-from tests.mock_file import true_returner
 
 
-@patch("api.workflows.views.get_workflow_by_id", new=true_returner)
+async def test_read_workflow(*args, **kwargs):
+    return {"id": 1, "title": "some"}
+
+
+@patch("api.workflows.views.get_workflow_by_id", new=test_read_workflow)
 @pytest.mark.asyncio
 async def test_read_workflow(client: TestClient):
     response = client.get("/workflows/read/1")
