187adf8cdb0cf773cab3875fdd4c9b38a0032111
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-26 17:08:50 +0300

    test was added

---
+++ tests/mock_approach/workflows/test_create_workflow_with_nodes.py
@@ -0,0 +1,30 @@
+import unittest
+from unittest.mock import patch
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.workflows.utils import create_workflow_with_nodes
+from api.workflows.schemas import WorkflowCreate
+from core.models import Workflow, Node
+
+
+class TestCreateWorkflowWithNodes(unittest.IsolatedAsyncioTestCase):
+    @patch(
+        "api.workflows.utils.create_node",
+        return_value=Node(id=1, type="Start Node"),
+    )
+    @patch(
+        "api.workflows.utils.create_workflow",
+        return_value=Workflow(id=1, title="title"),
+    )
+    async def test_create_workflow_with_nodes(
+        self, mock_create_workflow, mock_create_node
+    ):
+        result = await create_workflow_with_nodes(
+            session=AsyncSession(), workflow_in=WorkflowCreate(title="title")
+        )
+
+        # Asserting that the result is an instance of Workflow
+        self.assertIsInstance(result, Workflow)
+        # Asserting that create_workflow and create_node were called once
+        mock_create_workflow.assert_called_once()
+        self.assertEqual(mock_create_node.call_count, 2)
