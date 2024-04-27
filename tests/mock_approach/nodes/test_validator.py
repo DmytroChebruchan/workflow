b698531134661b2c3d6a7c2b823db0583ee7da04
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-27 13:43:25 +0300

    refactoring

--- tests/mock_approach/nodes/test_validator.py
+++ tests/mock_approach/nodes/test_validator.py
@@ -1,25 +1,23 @@
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import pytest
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.nodes.validator import ensure_unique_node_type
+from tests.mock_file import true_returner_mock
 
 
-async def test_ensure_unique_node_type():
-    # Mocking get_nodes_by_type
-    with patch("api.nodes.validator.get_nodes_by_type") as get_nodes_by_type:
-        get_nodes_by_type.return_value = True
-
-        # Check if exception is raised
-        with pytest.raises(HTTPException) as exc_info:
-            await ensure_unique_node_type(
-                node_type="Start Node", workflow_id=1, session=AsyncSession()
-            )
-
-        # Check the exception message or status code if needed
-        assert exc_info.value.status_code == status.HTTP_400_BAD_REQUEST
-        assert (
-            str(exc_info.value) == "400: Workflow with ID 1 has a Start Node"
+@patch(
+    "api.nodes.validator.get_nodes_by_type", return_value=true_returner_mock
+)
+async def test_ensure_unique_node_type(get_nodes_by_type):
+    # Check if exception is raised
+    with pytest.raises(HTTPException) as exc_info:
+        await ensure_unique_node_type(
+            node_type="Start Node", workflow_id=1, session=AsyncSession()
         )
+
+    # Check the exception message or status code if needed
+    assert exc_info.value.status_code == status.HTTP_400_BAD_REQUEST
+    assert str(exc_info.value) == "400: Workflow with ID 1 has a Start Node"
