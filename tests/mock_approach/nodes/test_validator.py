703e09b4be00227dbf38a89a18ac6f215c9e6fc5
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-27 13:47:51 +0300

    refactoring

--- tests/mock_approach/nodes/test_validator.py
+++ tests/mock_approach/nodes/test_validator.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 import pytest
 from fastapi import HTTPException, status
@@ -26,39 +26,29 @@ async def test_ensure_unique_node_type(get_nodes_by_type):
     assert str(exc_info.value) == "400: Workflow with ID 1 has a Start Node"
 
 
-from unittest.mock import MagicMock, patch
-from sqlalchemy.ext.asyncio import AsyncSession
-import pytest
-
-
 @pytest.mark.asyncio
-async def test_check_node_type_existence_in_workflow():
-    # Mocking the necessary functions
-    with patch(
-        "api.nodes.validator.get_workflow_by_id"
-    ) as mock_get_workflow_by_id, patch(
-        "api.nodes.validator.ensure_unique_node_type"
-    ) as mock_ensure_unique_node_type:
+@patch("api.nodes.validator.get_workflow_by_id")
+@patch("api.nodes.validator.ensure_unique_node_type")
+async def test_check_node_type_existence_in_workflow(
+    mock_ensure_unique_node_type, mock_get_workflow_by_id
+):
+    # Define the input data
+    node_model_dict = {"workflow_id": 1, "type": "Start Node"}
 
-        # Define the input data
-        node_model_dict = {"workflow_id": 1, "type": "Start Node"}
+    # Mocking AsyncSession
+    mock_session = MagicMock(spec=AsyncSession)
 
-        # Mocking AsyncSession
-        mock_session = MagicMock(spec=AsyncSession)
+    # Call the function
+    await check_node_type_existence_in_workflow(node_model_dict, mock_session)
 
-        # Call the function
-        await check_node_type_existence_in_workflow(
-            node_model_dict, mock_session
-        )
+    # Assert the number of calls to get_workflow_by_id
+    mock_get_workflow_by_id.assert_called_once_with(
+        workflow_id=1, session=mock_session
+    )
 
-        # Assert the number of calls to get_workflow_by_id
-        mock_get_workflow_by_id.assert_called_once_with(
-            workflow_id=1, session=mock_session
-        )
-
-        # Assert the number of calls to ensure_unique_node_type
-        mock_ensure_unique_node_type.assert_called_once_with(
-            node_type="Start Node",
-            workflow_id=1,
-            session=mock_session,
-        )
+    # Assert the number of calls to ensure_unique_node_type
+    mock_ensure_unique_node_type.assert_called_once_with(
+        node_type="Start Node",
+        workflow_id=1,
+        session=mock_session,
+    )
