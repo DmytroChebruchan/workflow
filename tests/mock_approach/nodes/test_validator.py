a3657b469e1e6a19741a4eed1fd25942e9245985
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-27 13:46:29 +0300

    test_check_node_type_existence_in_workflow was added

--- tests/mock_approach/nodes/test_validator.py
+++ tests/mock_approach/nodes/test_validator.py
@@ -4,7 +4,10 @@ import pytest
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.nodes.validator import ensure_unique_node_type
+from api.nodes.validator import (
+    ensure_unique_node_type,
+    check_node_type_existence_in_workflow,
+)
 from tests.mock_file import true_returner_mock
 
 
@@ -21,3 +24,41 @@ async def test_ensure_unique_node_type(get_nodes_by_type):
     # Check the exception message or status code if needed
     assert exc_info.value.status_code == status.HTTP_400_BAD_REQUEST
     assert str(exc_info.value) == "400: Workflow with ID 1 has a Start Node"
+
+
+from unittest.mock import MagicMock, patch
+from sqlalchemy.ext.asyncio import AsyncSession
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_check_node_type_existence_in_workflow():
+    # Mocking the necessary functions
+    with patch(
+        "api.nodes.validator.get_workflow_by_id"
+    ) as mock_get_workflow_by_id, patch(
+        "api.nodes.validator.ensure_unique_node_type"
+    ) as mock_ensure_unique_node_type:
+
+        # Define the input data
+        node_model_dict = {"workflow_id": 1, "type": "Start Node"}
+
+        # Mocking AsyncSession
+        mock_session = MagicMock(spec=AsyncSession)
+
+        # Call the function
+        await check_node_type_existence_in_workflow(
+            node_model_dict, mock_session
+        )
+
+        # Assert the number of calls to get_workflow_by_id
+        mock_get_workflow_by_id.assert_called_once_with(
+            workflow_id=1, session=mock_session
+        )
+
+        # Assert the number of calls to ensure_unique_node_type
+        mock_ensure_unique_node_type.assert_called_once_with(
+            node_type="Start Node",
+            workflow_id=1,
+            session=mock_session,
+        )
