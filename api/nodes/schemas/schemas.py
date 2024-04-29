e29a8048a02e5aa6fb09ac83820a5a86be37aec0
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-04-29 20:17:59 +0300

    update of msg. msg field only with msg node

--- api/nodes/schemas/schemas.py
+++ api/nodes/schemas/schemas.py
@@ -8,7 +8,6 @@ from api.nodes.node_attr_values import NodeType
 class NodeBase(BaseModel):
     type: NodeType
     workflow_id: int
-    message_text: Optional[str] = None
     status: Optional[str] = None
     condition: Optional[str] = None
     model_config = ConfigDict()
@@ -18,14 +17,17 @@ class NodeCreate(NodeBase):
     from_node_id: Optional[int] | None = None
     nodes_dest_dict: Optional[dict] | None = None
     edge_condition_type: Optional[bool] = True
+    message_text: Optional[str] = None
 
 
 class NodeUpdate(NodeBase):
     id: int
+    message_text: Optional[str] = None
     from_node_id: Optional[int] | None = None
     nodes_dest_dict: List[dict] | list = []
 
 
 class NodeFromDB(NodeBase):
     id: int
+    message_text: Optional[str] = None
     model_config = ConfigDict(from_attributes=True)
