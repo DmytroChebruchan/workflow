c602c908424cf19796b2c383eb77d073254e978a
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-05-02 14:32:18 +0300

    db is not created by running the migration

--- core/database/database.py
+++ core/database/database.py
@@ -13,9 +13,6 @@ SessionLocal = async_sessionmaker(engine)
 
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
     db = SessionLocal()
     try:
         yield db
