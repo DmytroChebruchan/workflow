ea7b0570e76ebaead74048537af6427af1cdc857
DmitriyChebruchan <chebruchan2707@gmail.com>
2024-05-08 17:23:26 +0300

    adding logs

--- demo_script.py
+++ demo_script.py
@@ -7,8 +7,8 @@ from sqlalchemy.ext.asyncio import (
 )
 from starlette.testclient import TestClient
 
+from demo.samples_of_nodes import nodes_to_create, nodes_to_update
 from main import app
-from demo.samples_of_nodes import nodes_to_update, nodes_to_create
 
 engine = create_async_engine("sqlite+aiosqlite:///db.sqlite3")
 SessionLocal = async_sessionmaker(engine)
@@ -26,38 +26,39 @@ def create_workflow():
     client = TestClient(app)
     # create workflow
     client.post("/workflows/create/", json={"title": "Test Workflow"})
+    print("Workflow was created with Start and End nodes.")
 
 
 def create_nodes(nodes: list):
     client = TestClient(app)
     for node in nodes:
         client.post("nodes/create/", json=node)
-        print(f"{node.type} was created.")
+        print(f"{node['type']} was created.")
 
 
 def update_nodes(nodes):
     client = TestClient(app)
     for node in nodes:
-        client.post(f"nodes/update/{node.id}", json=node)
-        print(f"{node.type} was updated.")
+        client.put(f"nodes/update/{node['id']}", json=node)
+        print(f"{node['type']} was updated to create edges to End Node.")
 
 
 def run_workflow():
     client = TestClient(app)
     result = client.get("/workflows/run/1")
-    print(result.content)
+    print(f"Path:\n {result.content}")
 
 
 def delete_workflow():
     client = TestClient(app)
     client.delete("/workflows/delete/1")
-    print("workflow was deleted")
+    print("Workflow was deleted.")
 
 
 if __name__ == "__main__":
     # creating workflow
     create_workflow()
-
+    #
     # creating nodes related
     create_nodes(nodes_to_create)
 
